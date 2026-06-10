975a7299559c87522e814780c9e2cef853364a78
Endermen9932 <mosskat@tutanota.de>
2026-06-10 07:56:26 +0200

    fix: downgrade to Kotlin 2.0.0 + KSP 2.0.0-1.0.24, safe known-working versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,9 +18,16 @@ android {
     }
 
     buildTypes {
+        debug {
+            applicationIdSuffix = ""
+            isDebuggable = true
+        }
         release {
             isMinifyEnabled = false
-            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
